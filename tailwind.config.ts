fe270606b4f4191ac450360526c9187b2b0d17e8
seannxh <nohsean6@gmail.com>
2025-01-25 04:05:34 -0600

    Dark / Light Mode Going to Sleep

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,58 @@
 import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors";
+import colors from "tailwindcss/colors";
+import { createTheme } from "@mui/system";
+
+const base =[
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+]
+
+const shade = {
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "500",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+
+const generateThemeObject = (colors: any, mapping: any, invert = false) => {
+  const theme: any = {};
+  base.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key, value]: any) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    });
+  });
+  return theme;
+};
+
+const lightTheme = generateThemeObject(colors, shade);
+const darkTheme = generateThemeObject(colors, shade, true);
+
+const themes = {
+  light: {
+    ...lightTheme,
+    white: "#ffffff",
+  },
+  dark: {
+    ...darkTheme,
+    white: colors.gray["950"],
+    black: colors.gray["50"],
+  },
+};
 
 export default {
   content: [
@@ -14,5 +68,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [createThemes(themes)],
 } satisfies Config;
